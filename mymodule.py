c17e6b49fd1135e996d2d56025245cdee49a33c3
Lee-Security <needyou_20@naver.com>
2025-09-23 15:48:17 +0900

    new

--- mymodule.py
+++ mymodule.py
@@ -3,3 +3,13 @@ def my_add(a, b):
 
 def my_sub(a, b):
     return a - b
+
+if __name__ == "__main__": # 할당값을 줘서 안전장치 기능을 시킴
+    print(my_add(1, 2))
+    print(my_sub(3, 4))
+
+print(f"mymodule안에서의 __name__: {__name__}")
+
+#모듈을 개발할 때. 기능을 테스트하고 모듈을 쓴다고 했을 때
+#(원치 않는 결과인 쓰레기 값이 포함될 것 같은 경우) 
+#if __name__ == "__main__": 를 기입
